7ef3ddea83f5c26c86ae909f429cba3e1c22db4e
vgdatco2oo2@gmail.com <SPC@21-LAB402>
2024-09-17 20:20:32 +0700

    Them bai 5

--- Java1-1/src/Bai4/Bai4_PhepTinhCoBan.java
+++ Java1-1/src/Bai4/Bai4_PhepTinhCoBan.java
@@ -2,11 +2,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package Bai4;
 
+import java.util.Scanner;
 /**
  *
  * @author SPC
  */
 public class Bai4_PhepTinhCoBan {
+    public static void main(String[] args) {
+        System.out.println("Tinh Cong Tru Nhan Chia");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Nhap so a: ");
+        int a = sc.nextInt();
+        
+        System.out.println("Nhap so b: ");
+        int b = sc.nextInt();
+        
+        System.out.println("KQ = " + tinhthuong(a, b));
+    }
+    
+    public static int tinhtong(int a, int b){
+        return a + b;
+    }
+    public static int tinhhieu(int a, int b){
+        return a - b;
+    }
+    public static int tinhtich(int a, int b){
+        return a * b;
+    }
+    public static String tinhthuong(int a, int b){
+        if(b == 0){
+            return "Khong the chia voi 0";
+        }
+        else{
+            return Double.toString(a / b);
+        }
+        
+    }
     
 }
---
+++ Java1-1/src/Bai5/Bai5_TinhTuoi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bai5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SPC
+ */
+public class Bai5_TinhTuoi {
+    public static void main(String[] args) {
+        
+    }
+}
